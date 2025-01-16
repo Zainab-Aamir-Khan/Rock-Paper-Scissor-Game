790a27450516b375295bdad53bc2e922ba5e073f
Zainab Aamir <zaamir427@gmail.com>
2025-01-16 22:34:18 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Rock Paper Scissors
+
+import random 
+#rock wins over scissors
+#scissors win over paper
+#paper wins over rock 
+print("Welcome To The Rock Paper Scissor Game!!!!!")
+
+guess = ["rock", "paper", "scissors"]
+
+while True:
+    userInput = input("enter your choice: ")
+
+    choice = random.choice(guess)
+    print("you chose ", userInput)
+    print("computer chose ", choice)
+
+    if userInput == choice:
+        print("It's a Tie!!!!")
+    elif userInput == "rock" and userInput == "scissors":
+        print("You won!!")
+    elif userInput == "scissors" and userInput == "paper":
+        print("you won!!!")
+    elif userInput == "paper" and userInput == "rock":
+        print("you won!!!")
+        break
+    else:
+     print("you lost the game")
